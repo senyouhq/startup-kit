9f0c48fe45696b489a70a1e90198cf02d875ffc1
Thet Win Aung <minthit21dj@gmail.com>
2021-04-07 20:25:56 +0900

    Update webpack config to remove root path

--- webpack.config.js
+++ webpack.config.js
@@ -130,7 +130,7 @@ module.exports = (env = {}, argv) => {
     output: {
       filename: '[name]-[hash].js',
       path: outputPath,
-      publicPath: '/',
+      // publicPath: '',
     },
     module: {
       rules: [
